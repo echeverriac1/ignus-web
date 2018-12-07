eaa16e94fe09fb04dec2720c9262040c454cb586
Carlos <echeverriacarlos1@gmail.com>
2018-12-07 07:07:47 -0400

    navigation and footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AgencyService } from '../../services/agency.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
 
-  constructor() { }
+export class FooterComponent implements OnInit {
+  agency: any;
+
+  constructor(public _agencyService: AgencyService) {
+    this.agency=[];
+  }
 
   ngOnInit() {
+    this._agencyService.getAgency()
+    .then((result) => {
+      this.agency=result['data'];
+    }, (err) => {
+      console.log(err);
+      //this.loader.dismiss();
+    });
+    console.log('police two');
+ 
   }
 
 }
--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AgencyService} from '../../services/agency.service'
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  agency: any;
 
-  constructor() { }
+  constructor(public _agencyService: AgencyService) {
+    this.agency=[];
+  }
 
   ngOnInit() {
+    this._agencyService.getAgency()
+    .then((result) => {
+      this.agency=result['data'];
+    }, (err) => {
+      console.log(err);
+      //this.loader.dismiss();
+    });
+    console.log('police two');
+ 
   }
 
 }
