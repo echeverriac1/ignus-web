423e4511bfa6a056fac7886e1caa41bc269627a1
Carlos <echeverriacarlos1@gmail.com>
2018-12-14 09:32:49 -0400

    not yet but is late

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -7,11 +7,25 @@ import { GlobalService } from '../../providers/global.service'
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
 
-  constructor( public _globalService: GlobalService ) { }
+export class RegisterComponent implements OnInit {
+  // agency: any;
+  cliente: any;
+
+  constructor( public _globalService: GlobalService ) {  
+    // this.agency=[];
+    this.cliente = [];
+  }
 
   ngOnInit() {
+    // this._globalService.getModel('/api/agency')
+    //  .then((result) => {
+    //   //console.log(result['data']);
+    //    this.agency=result['data'];
+    //  },(err) => {
+    //    console.log(err);
+    //  });
+
   }
 
   showAlert(){
@@ -19,13 +33,14 @@ export class RegisterComponent implements OnInit {
   }
 
   Registrar(){
+    console.log('trying');
 
     this._globalService.addModel(this._globalService,"user/client").then(data =>{
       this.showAlert();
     })
   
-    const data = JSON.parse(localStorage.getItem("client"));
-    console.log(data);
+    //const data = JSON.parse(localStorage.getItem("client"));
+    //console.log(data);
 
   }
 
