e5dd687ce4fe42451adcfe5fb394cf0307142a83
Carlos <echeverriacarlos1@gmail.com>
2018-12-14 09:11:29 -0400

    fuck, i can not, this is a bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { GlobalService } from './providers/global.service';
 
@@ -34,6 +34,7 @@ import { RegisterComponent } from './pages/register/register.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
   ],
   providers: [
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GlobalService } from '../../providers/global.service'
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _globalService: GlobalService ) { }
 
   ngOnInit() {
   }
+
+  showAlert(){
+    alert("Su registro ha sido exitoso");
+  }
+
+  Registrar(){
+
+    this._globalService.addModel(this._globalService,"user/client").then(data =>{
+      this.showAlert();
+    })
+  
+    const data = JSON.parse(localStorage.getItem("client"));
+    console.log(data);
+
+  }
+
 
 }
