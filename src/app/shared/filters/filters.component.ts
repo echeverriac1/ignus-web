6618a56235349ce7aa00d95aac3344388369f1a9
Carlos Echeverria <echeverriacarlos1@gmail.com>
2019-01-19 21:20:12 -0400

    carrousel modification and other thing like a filters or other services

--- src/app/shared/filters/filters.component.ts
+++ src/app/shared/filters/filters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GlobalService} from 'src/app/providers/global.service';
 
 @Component({
   selector: 'app-filters',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.component.css']
 })
 export class FiltersComponent implements OnInit {
+  typeServices: any;
+  typeProperties: any;
 
-  constructor() { }
+  constructor(public _globalService:GlobalService) { 
+    this.typeServices=[];
+    this.typeProperties=[];
+  }
+
+  getTypeServices(){
+    this._globalService.getModel('/api/typeService')
+     .then((result) => {
+       this.typeServices=result['data'];
+     },(err) => {
+       console.log(err);
+     });
+  }
+
+  getTypeOfProperty(){
+    this._globalService.getModel('/api/typeProperty')
+     .then((result) => {
+       this.typeProperties=result['data'];
+     },(err) => {
+       console.log(err);
+     });
+  }
+
 
   ngOnInit() {
-      }
+    this.getTypeServices();
+    this.getTypeOfProperty();
+  }
 
 }
