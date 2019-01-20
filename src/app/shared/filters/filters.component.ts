86e2ffee822e36deb5845f63d41a8e033c89d940
Carlos Echeverria <echeverriacarlos1@gmail.com>
2019-01-19 21:22:33 -0400

    giving commit to merge it

---
+++ src/app/shared/filters/filters.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../providers/global.service';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css']
+})
+export class FiltersComponent implements OnInit {
+  typeServices: any;
+  typeProperties: any;
+
+  constructor(public _globalService:GlobalService) { 
+    this.typeServices=[];
+    this.typeProperties=[];
+  }
+
+  getTypeServices(){
+    this._globalService.getModel('/api/typeService')
+     .then((result) => {
+       this.typeServices=result['data'];
+     },(err) => {
+       console.log(err);
+     });
+  }
+
+  getTypeOfProperty(){
+    this._globalService.getModel('/api/typeProperty')
+     .then((result) => {
+       this.typeProperties=result['data'];
+     },(err) => {
+       console.log(err);
+     });
+  }
+
+
+  ngOnInit() {
+    this.getTypeServices();
+    this.getTypeOfProperty();
+  }
+
+}
